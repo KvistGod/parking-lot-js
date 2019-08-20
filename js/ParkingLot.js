a56a292cf63cc0f374f43e62836457766f0e45e1
Kvist <chris@ikvistgaard.com>
2019-08-20 08:39:16 +0200

    Tilføjet Fakta, Bilka & Føtex.js og .html

--- js/ParkingLot.js
+++ js/ParkingLot.js
@@ -9,6 +9,10 @@ class ParkingLot {
     entranceGate = document.getElementById('entrance-gate');
     exitGate = document.getElementById('exit-gate');
 
+    constructor(calculatePrice){
+        this.calculatePrice = pcalculatePrice;
+    }
+
     checkin(licensePlate) {
         if (this.checkedInCars[licensePlate] != undefined) {
             throw new Error(`${licensePlate} holder allerede på pladsen!`);
@@ -23,9 +27,8 @@ class ParkingLot {
         if (checkinTime == undefined || checkinTime.constructor != Date) {
             throw new Error(`${licensePlate} holder ikke på pladsen!`);
         } else {
-            const time = Math.round((new Date() - checkinTime)/1000);
-            this.checkedInCars[licensePlate] = 15 * (Math.floor(time / 15) + 1);
-            return this.checkedInCars[licensePlate];
+            this.calculatePrice
+            
         }
     }
 
